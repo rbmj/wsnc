9f7bb1ce941df8e10599be1766d1da55b116afc9
rbmj <rbmj@verizon.net>
2016-06-24 15:43:31 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+wsnc
+-----
+
+`wsnc` is an application that aims to be as close of a replacement as possible
+for `nc` that uses websockets as its transport layer.
+
+Dependencies:
+ - Python 3.4+
+ - Autobahn
---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+import sys
+
+if not (sys.version_info[0] >= 3 and sys.version_info[1] >= 4):
+    sys.exit('Requires Python >= 3.4')
+
+setup(
+    name='wsnc',
+    version='0.0.1',
+    license='ISC License',
+    description='netcat over websockets',
+    author='Blair Mason',
+    url='https://github.com/rbmj/wsnc',
+    scripts='wsnc/wsnc',
+    install_requires=['autobahn'])
